8c20b4b24f3a77a7cb61032333b6276531ddb7ae
shad <=>
2016-08-31 17:20:20 +0300

    Add AbstractUserAction class.  Class that handles shared dependancy between all user action

---
+++ src/main/java/com/newssite/action/user/AbstractUserAction.java
@@ -0,0 +1,26 @@
+package com.newssite.action.user;
+
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.newssite.service.UserService;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
+import com.opensymphony.xwork2.validator.annotations.StringLengthFieldValidator;
+
+@ParentPackage(value="user")
+public abstract class AbstractUserAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -231851388886342994L;
+
+	protected UserService service;
+
+	@Autowired
+	public void setService(UserService service) {
+		this.service = service;
+	}
+	
+}
