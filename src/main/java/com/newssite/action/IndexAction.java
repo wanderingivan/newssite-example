87b57b4f9fb659e6cd5ac976e5e53668cbdee40c
shad <=>
2016-08-31 17:06:50 +0300

    Add index action and basic search action

---
+++ src/main/java/com/newssite/action/IndexAction.java
@@ -0,0 +1,200 @@
+package com.newssite.action;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.util.SubsetIteratorFilter.Decider;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.newssite.action.article.BaseArticleAction;
+import com.newssite.model.Article;
+import com.newssite.service.ArticleService;
+import com.newssite.service.MessageService;
+import com.opensymphony.xwork2.ActionSupport;
+
+@ParentPackage(value="index")
+@Results({@Result(name="success",type="tiles",location="indexLayout")})
+public class IndexAction extends BaseArticleAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8582563204335664300L;
+	private static final Logger logger = Logger.getLogger(IndexAction.class);
+
+	
+	
+	private List<Article> carousel;
+	private List<Article> category1;
+	private List<Article> category2;
+	private List<Article> category3;
+	private List<Article> category4;
+
+	private String carouselCategory;
+	
+	@Action(value="latest")
+	public String execute(){
+		try{
+			populateArticles(new String[]{"latest",
+					 					  "politics",
+					 					  "international",
+					 					  "sports",
+					 					  "entertainment"},service.latestCategories());
+			setCarouselCategory("latest");
+			return SUCCESS;
+		}catch(Exception e){
+			System.out.println(e.getCause() + "\n" + e.getMessage());
+			e.printStackTrace();
+			logger.error("Error loading articles in indexAction "+ e);
+		}
+		return ERROR;
+	}
+ 
+
+	@Action(value="politics")
+	public String politics(){
+		try{
+			populateArticles(new String[]{"politics",
+										  "international",
+										  "sports",
+										  "entertainment",
+										   null},service.latestCategories());
+			setCarouselCategory("politics");
+			return SUCCESS;
+		}catch(Exception e){
+			logger.error("Error loading articles in indexAction "+ e);
+			logger.error(e.getStackTrace());
+		}
+		return ERROR;
+	}
+	 
+	@Action(value="international")
+	public String international(){
+		try{
+			populateArticles(new String[]{"international",
+										  "politics",
+										  "sports",
+										  "entertainment",
+			                               null},service.latestCategories());
+			setCarouselCategory("international");
+			return SUCCESS;
+		}catch(Exception e){
+			logger.error("Error loading articles in indexAction "+ e);
+			logger.error(e.getStackTrace());
+		}
+		return ERROR;
+	}
+  
+	@Action(value="sports")
+	public String sports(){
+		try{
+			populateArticles(new String[]{"sports",
+										  "entertainment",
+										  "international",
+										  "politics",
+			                               null},service.latestCategories());
+			setCarouselCategory("sports");
+			return SUCCESS;
+		}catch(Exception e){
+			logger.error("Error loading articles in indexAction "+ e);
+			logger.error(e.getStackTrace());
+		}
+		return ERROR;
+	}
+  
+	@Action(value="entertainment")
+	public String entertainment(){
+		try{
+			populateArticles(new String[]{"entertainment",
+										  "sports",
+										  "international",
+										  "politics",
+			                               null},service.latestCategories());
+			setCarouselCategory("entertainment");
+			return SUCCESS;
+		}catch(Exception e){
+			logger.error("Error loading articles in indexAction "+ e);
+			logger.error(e.getStackTrace());
+		}
+		return ERROR;
+	}
+ 
+	/**
+	 * Populates article lists based on ordering of categories 
+	 * @param ordering
+	 * @param articles
+	 */
+	private void populateArticles(String[] ordering,Map<String,List<Article>> articles){
+		carousel = articles.get(ordering[0]);
+		category1 = articles.get(ordering[1]);
+		category2 = articles.get(ordering[2]);
+		category3 = articles.get(ordering[3]);
+		category4 = articles.get(ordering[4]);
+	}
+	
+	
+	public List<Article> getCarousel() {
+		return carousel;
+	}
+
+
+	public void setCarousel(List<Article> carousel) {
+		this.carousel = carousel;
+	}
+
+
+	public List<Article> getCategory1() {
+		return category1;
+	}
+
+
+	public void setCategory1(List<Article> category1) {
+		this.category1 = category1;
+	}
+
+
+	public List<Article> getCategory2() {
+		return category2;
+	}
+
+
+	public void setCategory2(List<Article> category2) {
+		this.category2 = category2;
+	}
+
+
+	public List<Article> getCategory3() {
+		return category3;
+	}
+
+
+	public void setCategory3(List<Article> category3) {
+		this.category3 = category3;
+	}
+
+
+	public List<Article> getCategory4() {
+		return category4;
+	}
+
+
+	public void setCategory4(List<Article> category4) {
+		this.category4 = category4;
+	}
+
+
+	public String getCarouselCategory() {
+		return carouselCategory;
+	}
+
+
+	public void setCarouselCategory(String carouselCategory) {
+		this.carouselCategory = carouselCategory;
+	}
+	
+}
---
+++ src/main/java/com/newssite/action/SearchAction.java
@@ -0,0 +1,128 @@
+package com.newssite.action;
+
+import com.newssite.model.Article;
+import com.newssite.model.User;
+import com.newssite.service.ArticleService;
+import com.newssite.service.UserService;
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.ParentPackage;
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Provides methods for searching 
+ * trough users and products based on query type.
+ *
+ */
+@ParentPackage(value="index")
+public class SearchAction extends ActionSupport{
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3367089994085106440L;
+	private static final Logger logger = Logger.getLogger(SearchAction.class);
+	
+	private static final String article = "article",
+								user = "user";
+	
+	private UserService userService;
+	
+	private ArticleService articleService;
+	
+	private List<Article> articles;
+	private Map<String, String> users;
+	
+	private String query,type,message;
+	private boolean missing;
+	
+	@Action(value="search",results={@Result(name="article", type="tiles", location="articleListLayout"),
+								    @Result(name="user",    type="tiles", location="userListLayout")})
+	public String execute(){
+		logger.debug("Searching database with params " + query + " " + type);
+		try{
+			if(type.equals(article)){
+				if(isMissing()){setMessage(getText("global.missing_article"));}
+				else{setMessage(getText("global.articles_matching"));}
+				articles = articleService.findArticles(query);
+				return article;
+			}else if(type.equals(user)){
+				if(isMissing()){setMessage(getText("global.missing_user"));}
+				else{setMessage(getText("global.users_matching"));}
+				users = userService.findUsers(query);
+				return user;
+			}else{
+				throw new IllegalArgumentException("Unmapped query type: "+type);
+			}
+		}catch(Exception e){
+			logger.error(String.format("Exception caught searching for query %s with type %s \n %s",query,type,e));
+		}
+		return ERROR;
+	}
+
+	public List<Article> getArticles() {
+		return articles;
+	}
+
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+
+	public Map<String, String> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Map<String, String> users) {
+		this.users = users;
+	}
+
+	public String getQuery() {
+		return query;
+	}
+
+	public void setQuery(String query) {
+		this.query = query;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public boolean isMissing() {
+		return missing;
+	}
+
+	public void setMissing(boolean missing) {
+		this.missing = missing;
+	}
+
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+
+	@Autowired
+	public void setArticleService(ArticleService service) {
+		this.articleService = service;
+	}
+	
+}
