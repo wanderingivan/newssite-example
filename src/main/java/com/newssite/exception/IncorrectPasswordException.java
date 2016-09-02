4cbc5cd0e6202bca5785e0c2b5f87a3963253b46
shad <=>
2016-09-02 14:31:06 +0300

    Add Exception classes and ConstraintExceptionConverter

---
+++ src/main/java/com/newssite/exception/IncorrectPasswordException.java
@@ -0,0 +1,10 @@
+package com.newssite.exception;
+
+public class IncorrectPasswordException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3299038069397595244L;
+
+}
