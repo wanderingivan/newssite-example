0c2ed4dc61687a03921c70a9f7c0e5c013f89133
shad <=>
2016-09-02 13:13:00 +0300

    Add ImageService - interface that provides methods for loading dynamic images. 	* ImageServiceImpl implmentation that provides caching and delegates to utility.

---
+++ src/main/java/com/newssite/service/ImageService.java
@@ -0,0 +1,37 @@
+package com.newssite.service;
+
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * 
+ * Service Layer interface providing
+ * basic save and retrieve operations
+ * for images.
+ * @throws IOExceptions when missing files
+ * or lacking credentials to save in directories 
+ */
+public interface ImageService {
+
+	/**
+	 * Retrieve an image as a base 64 string
+	 * @param path of the file to use
+	 * @return an image converted to a base 64 string
+	 * @throws IOException
+	 */
+	String getB64(String path);
+	
+	byte [] loadImage(String path); 
+	
+	
+	
+	/**
+	 * Saves an image to a predefined destination
+	 * @param file
+	 * @param contentType
+	 * @param fileName
+	 * @throws IOException
+	 */
+	void saveImage(File file,String contentType,String fileName) throws IOException;
+}
