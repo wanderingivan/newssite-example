de00501c0d6f12bd23d22cd14159c6acbccbde72
shad <=>
2016-09-02 13:14:32 +0300

    Add MessageService - interface that provides methods for handling comments,messages and tasks.  * MessageServiceImpl - implementation that provides transaction support

---
+++ src/main/java/com/newssite/service/MessageService.java
@@ -0,0 +1,161 @@
+package com.newssite.service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.SortedSet;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import com.newssite.model.Chat;
+import com.newssite.model.Comment;
+import com.newssite.model.Message;
+import com.newssite.model.Task;
+
+
+/**
+ * Service Layer interface providing
+ * basic operations for messages,comments
+ * and admin tasks
+ * Defines method security checks.
+ * @see com.newssite.model.Message
+ * @see com.newssite.model.Task
+ * @see com.newssite.model.Comment
+ */
+public interface MessageService {
+	
+	/**
+	 * Adds a task for the specified user<br/>
+	 * The task will appear in a users pending tasks
+	 * and is visible to everyone with admin permission 
+	 * @param task the 
+	 * @param assigner username that creates the task
+	 * @param assignee 
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	void addTask(Task task, String username, String assigned);
+	
+	/**
+	 *  Sets a task as complete 
+	 * @param taskId id of the task in db
+	 * @param comment optional comment on task completion 
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	void completeTask(long task_id, String comment);
+
+	/**
+	 * Removes a task, 
+	 * @param taskId id of the task in db
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	void removeTask(long task_id);
+
+	/**
+	 * List most recent tasks. 
+	 * @return a collection of Task ordered by task creation date
+	 * @see Task
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	List<Task> latestTasks();
+	
+	/**
+	 * Loads all tasks. 
+	 * @param username the user whose assigned tasks will be fetched
+	 * @return a collection of Task ordered by task creation date
+	 * @see Task
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	List<Task> retrieveAllTasks();
+	
+	/**
+	 * Loads all tasks for a particular user.
+	 * @param username the user whose assigned tasks will be fetched
+	 * @return a collection of Task ordered by task creation date
+	 * @see Task
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	List<Task> retrieveUserTasks(String username);
+
+	/**
+	 * Loads recent unfinished tasks for a particular user.
+	 * @param username the user whose assigned tasks will be fetched
+	 * @return a collection of Task ordered by task creation date
+	 * @see Task
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	List<Task> retrieveUserPendingTasks(String username);
+	
+	/**
+	 * Returns the unfinished tasks count for the admin user. 
+	 * @param username
+	 * 
+	 * @return count
+	 */
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	int countPending(String username);
+	
+	@PreAuthorize("isAuthenticated()")
+	public void addComment(String message, Long articleId, String username);
+
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	public void editComment(String newMessage,Long commentId);	
+	
+	/**
+	 * Sends a messages between two users.
+	 * @param message
+	 * @param sender
+	 * @param receiver
+	 */
+	@PreAuthorize("isAuthenticated()")
+	public void sendMessage(String message, String sender, String receiver);
+
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	public void deleteComment(Long commentId);
+	
+	@PreAuthorize("hasRole='ROLE_ADMIN'")
+	public void deleteMessage(Long messageId);
+	
+	/**
+	 * Returns the unread  messages count for a user from the database
+	 * @param username
+	 * 
+	 * @return count
+	 */
+	@PreAuthorize("isAuthenticated()")
+	int countUnread(String username);
+	
+	/**
+	 * Returns a list  of unread messages for a user from the database.
+	 * The messages will be marked as read after retrieval.
+	 * @param user_id the id of the user 
+	 * @return a collection of Message ordered by date of post.
+	 * @see Message
+	 */
+	@PreAuthorize("isAuthenticated()")
+	List<Message> getUnread(String username);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN') or #username == authentication.name")
+	public SortedSet<Chat> retrieveUserChats(String username);
+
+	/**
+	 * Sends  message to a chat
+	 * @param sender the username of the sender
+	 * @param chatId the chat to add the message to 
+	 * @param message the message content to add
+	 * @return the message as a String[]
+	 */
+	@PreAuthorize("isAuthenticated()")
+	public String[] addMessage(String sender, long chatId, String message) throws IOException;
+	
+	public List<Comment> latestComments();
+	
+	public List<Comment> retrieveArticleComments(Long articleId);
+
+	public List<Comment> getComments(String ordering);
+	
+	public long upvote(int commentId);
+
+	public long downvote(int commentId);
+
+	public Chat getChat(String username,long chatId);
+
+}
---
+++ src/main/java/com/newssite/service/impl/MessageServiceImpl.java
@@ -0,0 +1,191 @@
+package com.newssite.service.impl;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.SortedSet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.newssite.dao.MessageDao;
+import com.newssite.service.ImageService;
+import com.newssite.service.MessageService;
+import com.newssite.model.Chat;
+import com.newssite.model.Comment;
+import com.newssite.model.Message;
+import com.newssite.model.Task;
+
+/**
+ * Implementation of UserService
+ * that provides transaction support
+ * and delegates CRUD options to a dao.
+ * @see com.pollapp.dao.MessageDao
+ *
+ */
+@Service
+public class MessageServiceImpl implements MessageService {
+
+	private final static String POPULAR = "popular",
+			                    LATEST = "latest";
+	
+	private MessageDao messageDao;
+	private ImageService imageService;
+	
+	
+	@Autowired
+	public MessageServiceImpl(MessageDao messageDao,ImageService imageService) {
+		super();
+		this.messageDao = messageDao;
+		this.imageService = imageService;
+	}
+
+	@Override
+	@Transactional
+	public void addComment(String message, Long articleId, String username) {
+		messageDao.addComment(message, articleId, username);
+	}
+	
+	@Override
+	@Transactional
+	public void editComment(String newMessage, Long commentId) {
+		messageDao.editComment(newMessage, commentId);
+	}
+
+	@Override
+	@Transactional
+	public List<Comment> retrieveArticleComments(Long articleId) {
+		return messageDao.retrieveArticleComments(articleId);
+	}
+	
+	@Override
+	@Transactional
+	public List<Comment> latestComments() {
+		return messageDao.getLatestComments();
+	}
+
+	@Override
+	@Transactional
+	public void deleteComment(Long commentId) {
+		messageDao.deleteComment(commentId);
+	}
+	
+	@Override
+	@Transactional
+	public void sendMessage(String message, String sender, String receiver) {
+		messageDao.sendMessage(message,sender,receiver);
+	}
+
+	@Override
+	@Transactional
+	public void deleteMessage(Long messageId) {
+		messageDao.deleteMessage(messageId);
+	}
+
+	@Override
+	@Transactional
+	public SortedSet<Chat> retrieveUserChats(String username) {
+		return messageDao.retrieveUserChats(username);
+	}
+
+	@Override
+	@Transactional
+	public String [] addMessage(String sender, long chatId, String message) throws IOException {
+		
+		Message saved = messageDao.addMessage(sender,chatId,message);
+		String[] result = new String[]{saved.getPoster().getUsername(),
+				                       imageService.getB64(saved.getPoster().getImagePath()),
+				                       saved.getMessage()};
+		return result;
+	}
+
+	@Override
+	@Transactional
+	public long upvote(int commentId) {
+		return messageDao.voteIncrement(commentId,1);
+	}
+
+	@Override
+	@Transactional
+	public long downvote(int commentId) {
+		return messageDao.voteIncrement(commentId,-1);
+	}
+
+	@Override
+	@Transactional
+	public Chat getChat(String username,long chatId) {
+		return messageDao.retrieveChat(chatId);
+	}
+
+	@Override
+	@Transactional
+	public List<Comment> getComments(String ordering) {
+		if(ordering.equals(POPULAR)){
+			return messageDao.getPopularComments();
+		}else if(ordering.equals(LATEST)){
+			return messageDao.getLatestComments();
+		}else{
+			throw new IllegalArgumentException(ordering);
+		}
+	}
+
+	@Override
+	@Transactional
+	public void addTask(Task task, String username, String assigned) {
+		messageDao.addTask(task, username, assigned);
+	}
+
+	@Override
+	@Transactional
+	public void completeTask(long taskId, String comment) {
+		messageDao.completeTask(taskId, comment);
+	}
+
+	@Override
+	@Transactional
+	public void removeTask(long taskId) {
+		messageDao.removeTask(taskId);
+	}
+
+	@Override
+	@Transactional
+	public List<Task> latestTasks() {
+		return messageDao.getTasks(false);
+	}
+
+	@Override
+	@Transactional
+	public List<Task> retrieveAllTasks(){
+		return messageDao.getTasks(true);
+	}
+	
+	@Override
+	@Transactional
+	public List<Task> retrieveUserPendingTasks(String username){
+		return messageDao.getTasks(username, false);
+	}
+	
+	@Override
+	@Transactional
+	public List<Task> retrieveUserTasks(String username) {
+		return messageDao.getTasks(username, true);
+	}
+
+	@Override
+	@Transactional
+	public int countPending(String username) {
+		return messageDao.countPending(username);
+	}
+
+	@Override
+	@Transactional
+	public int countUnread(String username) {
+		return messageDao.countUnread(username);
+	}
+
+	@Override
+	@Transactional
+	public List<Message> getUnread(String username) {
+		return messageDao.getUnread(username);
+	}
+}
