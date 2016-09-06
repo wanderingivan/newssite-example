83fafaa74c3c8e891fc7ecf403454c7ae83ecdf4
shad <=>
2016-09-06 22:19:39 +0300

    Add AuthInterceptor - Interceptor that inserts authenticated users in actions that implement AuthenticatedUserAware interface.

---
+++ src/main/java/com/newssite/interceptor/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package com.newssite.interceptor;
+
+import org.apache.log4j.Logger;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+public class AuthInterceptor extends AbstractInterceptor {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6165549166976305137L;
+	private static final Logger logger = Logger.getLogger(AuthInterceptor.class);
+
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		if(invocation.getAction() instanceof AuthenticatedUserAware){
+			if(logger.isDebugEnabled()){
+				logger.debug("Auth Interceptor Intercepting action " + invocation.getAction().getClass().getSimpleName() + "\n Inserting user" + getUserFromSecurityContext());
+			}
+			((AuthenticatedUserAware) invocation.getAction()).setUser(getUserFromSecurityContext());
+		}
+		return invocation.invoke();
+	}
+
+	private String getUserFromSecurityContext(){
+		
+		return SecurityContextHolder.getContext()
+									.getAuthentication()
+									.getName();
+	}
+}
